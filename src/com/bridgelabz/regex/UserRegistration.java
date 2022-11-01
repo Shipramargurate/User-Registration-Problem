587c981942d0362bcb63688450739ec910bc0fdd
shipramargurate <shivanikumari22322@gmail.com>
2022-11-01 12:11:30 +0530

    [Shivani]: Added Functionality of valid Email Id

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -33,12 +33,24 @@ public class UserRegistration {
 		}
 
 	}
+    public void validEmailId() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter email id");
+        String email = sc.nextLine();
 
+        boolean validEmail = Pattern.matches("^[a-z]*.[a-z]+@[a-z]+.[a-z]{2,3}(.[a-z]{2,})*$", email);
+        if (validEmail) {
+            System.out.println("Email is valid");
+        } else {
+            System.out.println("Email is invalid");
+        }
+    }
 	public static void main(String[] args) {
 		System.out.println("welcome to user registration program");
 		UserRegistration userRegistration = new UserRegistration();
 		// userRegistration.validFirstName();
-		userRegistration.validLastName();
+		//userRegistration.validLastName();
+		 userRegistration.validEmailId();
 	}
 
 }
