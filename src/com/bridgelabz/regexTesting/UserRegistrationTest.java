494b4d458343b5a33b7bf9e6905b3cc1a4341c42
shipramargurate <shivanikumari22322@gmail.com>
2022-11-05 17:13:44 +0530

    [Shivani]: Modified junit parametarised test

--- src/com/bridgelabz/regexTesting/UserRegistrationTest.java
+++ src/com/bridgelabz/regexTesting/UserRegistrationTest.java
@@ -1,13 +1,15 @@
 package com.bridgelabz.regexTesting;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import com.bridgelabz.regex.UserRegistration;
 
 import junit.framework.Assert;
 
 public class UserRegistrationTest {
-
-	  static UserRegistration userRegistration;
+	 static UserRegistration userRegistration;
 
 	    @BeforeClass
 	    public static void initialize(){
@@ -73,7 +75,22 @@ public class UserRegistrationTest {
 
 	    @Test
 	    public void givenPasswordWhenInValidShouldReturnFalse(){
-	        boolean result = userRegistration.validPassword("Shivanigmailcom");
+	        boolean result = userRegistration.validPassword("shivanigmailcom");
 	        Assert.assertFalse(result);
 	    }
+
+
+	    @ParameterizedTest
+	    @ValueSource(strings = {"Abc@yahoo.com","abc@gmail.com.com",})
+	    public void givenEmailWhenValidShouldReturnTrue(String email) {
+	        Assert.assertTrue(userRegistration.validEmailId(email));
+	    }
+
+	   @ParameterizedTest
+	    @ValueSource(strings = {"abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc@%*.com",})
+	    public void givenEmailWhenInValidShouldReturnFalse(String email) {
+	        Assert.assertFalse(userRegistration.validEmailId(email));
+	    }
+
+
 	}
